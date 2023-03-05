e5854c9c4704cad037a1b1d1d210ccc03e510e35
Senchurion <126983289+Senchurion@users.noreply.github.com>
2023-03-05 21:15:48 +0100

    Update chromosome_check.py

--- Python/8 kyu/chromosome_check.py
+++ Python/8 kyu/chromosome_check.py
@@ -1,2 +1,4 @@
+#https://www.codewars.com/kata/56530b444e831334c0000020
+
 def chromosome_check(sperm):
     return "Congratulations! You're going to have a daughter." if sperm == 'XX' else "Congratulations! You're going to have a son."
